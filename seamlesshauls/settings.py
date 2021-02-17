5db804fdef67fc604a3fbaf72277afd39fa5aac4
Green <greenakawushim@gmail.com>
2021-02-17 12:47:00 +0100

    Email Password changed

--- seamlesshauls/settings.py
+++ seamlesshauls/settings.py
@@ -138,7 +138,7 @@ STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'seamlesshauls@gmail.com'
-EMAIL_HOST_PASSWORD = 'Bracelet12345'
+EMAIL_HOST_PASSWORD = 'Bracelet1'
 
 EMAIL_USE_TLS = True
 
